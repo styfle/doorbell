be708fbefc692a1e5a5c15777704cd0e557dc3ff
Steven <steven@ceriously.com>
2020-04-26 15:51:32 -0400

    Add api

---
+++ api/ring.js
@@ -0,0 +1,22 @@
+const https = require('https');
+
+module.exports = (req, res) => {
+  const notification = `${req.query.name || 'Someone'} rang the doorbell.`;
+  const accessCode = process.env.ALEXA_ACCESS_CODE;
+  if (!accessCode) {
+    throw new Error('Did you forget to assign ALEXA_ACCESS_CODE?');
+  }
+  console.log(notification);
+
+  const url = new URL('https://api.notifymyecho.com/v1/NotifyMe');
+  url.searchParams.append('notification', notification);
+  url.searchParams.append('accessCode', accessCode);
+
+  https.get(url, ({ statusCode }) => {
+    if ([200, 201, 202].includes(statusCode)) {
+      res.end('Success');
+    } else {
+      res.end('Failed with status ' + statusCode);
+    }
+  });
+}
